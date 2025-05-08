390ad9c1b9271c7f24e981ded5983c4c0dfc6ee0
Prasanna Gonapinuwala <prasannaanura@yahoo.com>
2025-05-08 09:06:18 -0600

    how many guess

--- randomNum.py
+++ randomNum.py
@@ -1,9 +1,10 @@
 import random
 
 randomNumber = random.randint(1,10)
-
+guess = 0
 while True:
     guessNumber = int(input("Enter a number between 1 and 10 "))
+    guess+= 1
     if randomNumber == guessNumber:
         print("you win!")
         break
@@ -11,3 +12,4 @@ while True:
         print("Your guess too high!")
     else:
         print("Your guess low!")
+print(f"congragulation you win ! {guess} attempts")
