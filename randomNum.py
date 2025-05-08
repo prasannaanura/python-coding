68d60b4595b00047b871af04521d95cc407b53f1
Prasanna Gonapinuwala <prasannaanura@yahoo.com>
2025-05-08 00:37:20 -0600

    remove tracking

---
+++ randomNum.py
@@ -0,0 +1,13 @@
+import random
+
+randomNumber = random.randint(1,10)
+
+while True:
+    guessNumber = int(input("Enter a number between 1 and 10 "))
+    if randomNumber == guessNumber:
+        print("you win!")
+        break
+    elif randomNumber > guessNumber:
+        print(randomNumber,"Your guess too high!")
+    else:
+        print(randomNumber,"Your guess low!")
